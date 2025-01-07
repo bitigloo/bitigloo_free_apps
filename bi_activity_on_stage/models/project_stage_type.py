71962624ca8e47b4167849ac1b33a4b19bee041a
bitigloo-cabrel <cabrel@bitigloo.com>
2025-01-07 09:43:03 +0100

    [UPD] fix typo and migrate bi_activity_on_stage & bi_user_unassign_on_stage

---
+++ bi_activity_on_stage/models/project_stage_type.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+# ---------------------------------------------------------
+# Project Task Stage Inheritance
+# ---------------------------------------------------------
+
+
+class ProjectTaskType(models.Model):
+    _inherit = 'project.task.type'
+
+    activity_type = fields.Many2one(
+        "mail.activity.type", string="Activity Type",
+        domain=lambda self: ['|', ('res_model', '=', False), ('res_model', '=', 'project.project')]
+    )
+    specific_user = fields.Many2one("res.users", string="Specific User",
+                                    domain=lambda self: [('share', '=', False)])
+    user_type = fields.Selection(
+        [
+            ('project_manager', 'Project Manager'),
+            ('specific_user', 'Specific User')
+        ],
+        string="User Type"
+    )
